391118f3cf97beea4d94a97067ddc1e453712705
Ajay Chandran <ajay.chandran.1980@gmail.com>
2024-06-15 10:06:35 +0530

    Add Mailbox

---
+++ core/jvm/src/main/java/zio/internal/Mailbox.java
@@ -0,0 +1,95 @@
+package zio.internal;
+
+import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+
+abstract class MailboxClassPad implements Serializable {
+	protected int _0;
+	protected long _1;
+	protected long _2;
+	protected long _3;
+	protected long _4;
+	protected long _5;
+	protected long _6;
+}
+
+abstract class MailboxWrite extends MailboxClassPad {
+	protected transient volatile Mailbox.Node write;
+}
+
+abstract class MailboxWritePad extends MailboxWrite {
+	protected int __0;
+	protected long __1;
+	protected long __2;
+	protected long __3;
+	protected long __4;
+	protected long __5;
+	protected long __6;
+	protected long __7;
+}
+
+public final class Mailbox<A> extends MailboxWritePad {
+
+	private transient Node read;
+
+	public Mailbox() {
+		read = write = new Node(null);
+	}
+
+	public void add(A data) {
+		final Node next = new Node(data);
+		NEXT.setRelease(WRITE.getAndSet(this, next), next);
+	}
+
+	public boolean isEmpty() {
+		return null == read.next;
+	}
+
+	public boolean nonEmpty() {
+		return null != read.next;
+	}
+
+	@SuppressWarnings("unchecked")
+	public A poll() {
+		// Node next = (Node) (NEXT.getOpaque(read));
+		Node next = read.next;
+
+		if (next == null)
+			return null;
+
+		read = next;
+		final Object data = next.data;
+		next.data = null;
+		return (A) (data);
+	}
+
+	static class Node implements Serializable {
+
+		Object data;
+		volatile Node next;
+
+		Node(Object data) {
+			this.data = data;
+		}
+
+		Node(Object data, Node next) {
+			this.data = data;
+			this.next = next;
+		}
+	}
+
+	static final VarHandle NEXT;
+	static final VarHandle WRITE;
+
+	static {
+		try {
+			MethodHandles.Lookup lookup = MethodHandles.lookup();
+			NEXT = MethodHandles.privateLookupIn(Node.class, lookup).findVarHandle(Node.class, "next", Node.class);
+			WRITE = MethodHandles.privateLookupIn(MailboxWrite.class, lookup).findVarHandle(MailboxWrite.class, "write",
+					Node.class);
+		} catch (ReflectiveOperationException e) {
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+}
---
+++ core/native/src/main/scala/zio/internal/Mailbox.scala
@@ -0,0 +1,37 @@
+package zio.internal
+
+import zio.stacktracer.TracingImplicits.disableAutoTrace
+
+final class Mailbox[A] extends Serializable {
+
+  private[this] var read  = new Node(null)
+  private[this] var write = read
+
+  def add(data: A): Unit = {
+    val next = new Node(data.asInstanceOf[AnyRef])
+    write.next = next
+    write = next
+  }
+
+  def isEmpty(): Boolean =
+    null == read.next
+
+  def nonEmpty(): Boolean =
+    null != read.next
+
+  def poll(): A = {
+    val next = read.next
+
+    if (null == next)
+      return null.asInstanceOf[A]
+
+    val data = next.data
+    next.data = null
+    read = next
+    data.asInstanceOf[A]
+  }
+}
+
+private class Node(var data: AnyRef) {
+  var next: Node = _
+}
